3f657a42e6368578829e8c4f10aad769aa8cc40f
Jan-Gerrit Schettler-Köhler <jan-gerrit.schettler-koehler@capgemini.com>
2021-07-16 16:49:22 +0200

    Fixed solutions without image

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 function getParameters() {
-    const searchParams = new URLSearchParams(document.location.hash.length > 0? document.location.hash.substring(1):"");
+    const searchParams = new URLSearchParams(document.location.hash.length > 0 ? document.location.hash.substring(1) : "");
     var params = {};
     params.toString = () => searchParams.toString();
-    params.set = (k,v) => searchParams.append(k,v);
-    params.delete = (k,v) => {
+    params.set = (k, v) => searchParams.append(k, v);
+    params.delete = (k, v) => {
         var allValues = searchParams.getAll(k);
         var newAllValues = allValues.filter(av => av != v);
-        if(newAllValues.length != allValues.length){
+        if (newAllValues.length != allValues.length) {
             searchParams.delete(k);
-            newAllValues.forEach(av => searchParams.set(k,av));
+            newAllValues.forEach(av => searchParams.set(k, av));
             return true;
         }
-        return false; 
+        return false;
     }
     params.get = (k) => searchParams.getAll(k);
     params.keys = (k) => searchParams.keys(k);
     return params;
 }
 
-function search(){
+function search() {
     var parameters = getParameters();
     var solutions = solutionsJson;
     for (const filterName of parameters.keys()) {
         var filterValues = parameters.get(filterName);
-        for(var i in filterValues){
+        for (var i in filterValues) {
             var filterValue = filterValues[i];
             var filterSolutions = tagsJson[filterName][filterValue];
             var newSolutions = {};
-            for(var solutionKey in solutions){
-                if(filterSolutions.includes(solutionKey)){
+            for (var solutionKey in solutions) {
+                if (filterSolutions.includes(solutionKey)) {
                     newSolutions[solutionKey] = solutions[solutionKey];
                 }
             }
@@ -40,7 +40,7 @@ function search(){
     highlightSelected();
 }
 
-function renderSolutions(solutions){
+function renderSolutions(solutions) {
     var filterpanelbody = $("#resultspanel");
     filterpanelbody.empty();
     for (const solutionKey in solutions) {
@@ -50,11 +50,13 @@ function renderSolutions(solutions){
             var solutionHeadlineDiv = $('<div id="solution_' + solutionKey + '_headline" class="solutionheadline"></div>');
             var solutionHeadlineLink = $('<a id="solution_' + solutionKey + '_headline_link" class="solutionheadlinelink" href="' + solution.path + '"></a>');
             solutionHeadlineLink.text(solution.headline);
-            solutionHeadlineLink.attr('target','_blank')
+            solutionHeadlineLink.attr('target', '_blank')
             solutionHeadlineDiv.append(solutionHeadlineLink);
             solutionDiv.append(solutionHeadlineDiv);
-            var solutionImage = $('<img id="solution_' + solutionKey + '_image" class="solutionimage" src="' + solution.image + '"></img>');
-            solutionDiv.append(solutionImage);
+            if (solution.image != "") {
+                var solutionImage = $('<img id="solution_' + solutionKey + '_image" class="solutionimage" src="' + solution.image + '"></img>');
+                solutionDiv.append(solutionImage);
+            }
             var solutionSnippetDiv = $('<div id="solution_' + solutionKey + '_snippet" class="solutionsnippet"></div>');
             solutionSnippetDiv.text(solution.snippet);
             solutionDiv.append(solutionSnippetDiv);
@@ -63,14 +65,14 @@ function renderSolutions(solutions){
     }
 }
 
-function highlightSelected(){
+function highlightSelected() {
     $(".tag.selected").removeClass("selected");
     var parameters = getParameters();
     for (const filterName of parameters.keys()) {
         var filterValues = parameters.get(filterName);
-        for(var i in filterValues){
+        for (var i in filterValues) {
             var filterValue = filterValues[i];
-            $("#tag_" + filterName.replace(/[^a-zA-Z0-9]/g,"_") + '_' + filterValue.replace(/[^a-zA-Z0-9]/g,"_")).addClass("selected");
+            $("#tag_" + filterName.replace(/[^a-zA-Z0-9]/g, "_") + '_' + filterValue.replace(/[^a-zA-Z0-9]/g, "_")).addClass("selected");
         }
     }
 }
@@ -103,15 +105,15 @@ async function main() {
             filterpanel.append(filterpanelbody);
             for (const tagValue in tag) {
                 if (Object.hasOwnProperty.call(tag, tagValue)) {
-                    var tagDiv = $('<div class="tag" id="tag_' + filter.replace(/[^a-zA-Z0-9]/g,"_") + '_' + tagValue.replace(/[^a-zA-Z0-9]/g,"_") +'"></div>');
+                    var tagDiv = $('<div class="tag" id="tag_' + filter.replace(/[^a-zA-Z0-9]/g, "_") + '_' + tagValue.replace(/[^a-zA-Z0-9]/g, "_") + '"></div>');
                     tagDiv.text(tagValue);
-                    tagDiv.click((e) => { 
-                        if(!parameters.delete(filter, tagValue)){
+                    tagDiv.click((e) => {
+                        if (!parameters.delete(filter, tagValue)) {
                             parameters.set(filter, tagValue);
                         }
                         document.location.hash = "#" + parameters.toString()
                         search();
-                     });
+                    });
                     filterpanelbody.append(tagDiv);
                 }
             }
