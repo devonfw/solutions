39a57683718c3b083f6db0b99a6caca2881300eb
Yafang Bao <yafang.bao@capgemini.com>
2021-07-23 10:07:28 +0200

    Do not cache json files

--- index.js
+++ index.js
@@ -109,15 +109,15 @@ function disableFilters(solutions) {
 
 async function main() {
     indexJson = await $.ajax({
-        url: "index.json"
+        url: "index.json" + (Math.random()*10000)
     });
 
     solutionsJson = await $.ajax({
-        url: "solutions.json"
+        url: "solutions.json" + (Math.random()*10000)
     });
 
     tagsJson = await $.ajax({
-        url: "tags.json"
+        url: "tags.json" + (Math.random()*10000)
     });
 
 
