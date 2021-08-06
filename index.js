0f08a38203a616ccec0b8ff7a40d3fa779ab0238
yF-B <64614004+yF-B@users.noreply.github.com>
2021-08-06 12:56:01 +0200

    Pagination in architecture browser (#27)
    * Do not cache json files
    
    * Changed urls
    
    * paginate the results in the architecture Browser
    
    * changed variable name
    
    * updated
    
    * only loaded the data on the current page
    
    * changed pagination buttons
    
    * changed according to the comment
    
    * removed one global variable

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var pageSize = 10;
+
 function getParameters() {
     const searchParams = new URLSearchParams(document.location.hash.length > 0 ? document.location.hash.substring(1) : "");
     var params = {};
@@ -21,8 +23,14 @@ function getParameters() {
 function search() {
     var parameters = getParameters();
     var solutions = solutionsJson;
+    let curentPage = 1;
     for (const filterName of parameters.keys()) {
         var filterValues = parameters.get(filterName);
+        if (filterName == 'page') {
+            curentPage = filterValues[0];
+            parameters.delete('page', curentPage);
+            continue;
+        }
         for (var i in filterValues) {
             var filterValue = filterValues[i];
             var filterSolutions = tagsJson[filterName][filterValue];
@@ -35,37 +43,88 @@ function search() {
             solutions = newSolutions;
         }
     }
-    //console.log(solutions);
-    renderSolutions(solutions);
+    renderSolutions(solutions, curentPage);
     disableFilters(solutions);
     highlightSelected();
 }
 
-function renderSolutions(solutions) {
+function renderSolutions(solutions, current) {
     var filterpanelbody = $("#resultspanel");
     filterpanelbody.empty();
+
+    let currentPage = current;
+    let countSolutions = 0;
+    let dataSize = Object.keys(solutions).length;
+    let totalPage = Math.ceil(dataSize / pageSize);
+    let startData = (currentPage - 1) * pageSize + 1;
+    let endData = (currentPage * pageSize > dataSize ? dataSize : currentPage * pageSize);
+
     for (const solutionKey in solutions) {
         if (Object.hasOwnProperty.call(solutions, solutionKey)) {
-            const solution = solutions[solutionKey];
-            var solutionDiv = $('<div id="solution_' + solutionKey + '" class="solution"></div>');
-            var solutionHeadlineDiv = $('<div id="solution_' + solutionKey + '_headline" class="solutionheadline"></div>');
-            var solutionHeadlineLink = $('<a id="solution_' + solutionKey + '_headline_link" class="solutionheadlinelink" href="' + solution.path + '"></a>');
-            solutionHeadlineLink.text(solution.headline);
-            solutionHeadlineLink.attr('target', '_blank')
-            solutionHeadlineDiv.append(solutionHeadlineLink);
-            solutionDiv.append(solutionHeadlineDiv);
-            var solutionBodyDiv = $('<div id="solution_body_' + solutionKey + '" class="solutionbody"></div>');
-            if (solution.image != "") {
-                var solutionImage = $('<img id="solution_' + solutionKey + '_image" class="solutionimage" src="' + solution.image + '"></img>');
-                solutionBodyDiv.append(solutionImage);
+            countSolutions++;
+            if (countSolutions >= startData && countSolutions <= endData) {
+                const solution = solutions[solutionKey];
+                var solutionDiv = $('<div id="solution_' + solutionKey + '" class="solution"></div>');
+                var solutionHeadlineDiv = $('<div id="solution_' + solutionKey + '_headline" class="solutionheadline"></div>');
+                var solutionHeadlineLink = $('<a id="solution_' + solutionKey + '_headline_link" class="solutionheadlinelink" href="' + solution.path + '"></a>');
+                solutionHeadlineLink.text(solution.headline);
+                solutionHeadlineLink.attr('target', '_blank')
+                solutionHeadlineDiv.append(solutionHeadlineLink);
+                solutionDiv.append(solutionHeadlineDiv);
+                var solutionBodyDiv = $('<div id="solution_body_' + solutionKey + '" class="solutionbody"></div>');
+                if (solution.image != "") {
+                    var solutionImage = $('<img id="solution_' + solutionKey + '_image" class="solutionimage" src="' + solution.image + '"></img>');
+                    solutionBodyDiv.append(solutionImage);
+                }
+                var solutionSnippetDiv = $('<div id="solution_' + solutionKey + '_snippet" class="solutionsnippet"></div>');
+                solutionSnippetDiv.text(solution.snippet);
+                solutionBodyDiv.append(solutionSnippetDiv);
+                solutionDiv.append(solutionBodyDiv);
+                filterpanelbody.append(solutionDiv);
             }
-            var solutionSnippetDiv = $('<div id="solution_' + solutionKey + '_snippet" class="solutionsnippet"></div>');
-            solutionSnippetDiv.text(solution.snippet);
-            solutionBodyDiv.append(solutionSnippetDiv);
-            solutionDiv.append(solutionBodyDiv);
-            filterpanelbody.append(solutionDiv);
         }
     }
+
+    createBtns(totalPage, currentPage);
+}
+
+function createBtns(totalPage, current) {
+    $('#pagination').empty();
+    let pageSpan;
+    let currentPage = parseInt(current);
+    if (currentPage > 1) {
+        pageSpan = $("<span class='pageBtn' id='prepage' href=\"#\" data-page = " + (currentPage - 1) + "><  Precious</span>")
+        pageSpan.click(() => {
+            clickBtn(currentPage - 1);
+        });
+        $("#pagination").append(pageSpan);
+    }
+    for (let pageIndex = 1; pageIndex < totalPage + 1; pageIndex++) {
+        pageSpan = $("<a class='pageBtn' id='page" + pageIndex + "' data-page = " + (pageIndex) + "><span>" + pageIndex + "</span></a>");
+        pageSpan.click(() => {
+            clickBtn(pageIndex);
+        });
+        $("#pagination").append(pageSpan);
+    }
+    if (currentPage < totalPage) {
+        pageSpan = $("<span class='pageBtn' id='nextpage' href=\"#\"  data-page = " + (currentPage + 1) + ">Next  ></span>");
+        pageSpan.click(() => {
+            clickBtn(currentPage + 1);
+        });
+        $("#pagination").append(pageSpan);
+    }
+}
+
+function clickBtn(current) {
+    let parameters = getParameters();
+    let pageArr = parameters.get('page');
+    for (var p in pageArr) {
+        parameters.delete('page', pageArr[p]);
+    }
+    parameters.set('page', current);
+    document.location.hash = "#" + parameters.toString()
+    search();
+    $('#page' + (current)).css({ background: '#007bff', color: '#fff' });
 }
 
 function highlightSelected() {
@@ -163,7 +222,8 @@ async function main() {
     console.log(solutionsJson);
 
     $("head").append('<link href="https://fonts.googleapis.com/icon?family=Material+Icons+Sharp" rel="stylesheet">');
-
+    var paginationDiv = $('<div id="pagination" class = "pagination"></div>');
+    $("#content").append(paginationDiv);
     var parameters = getParameters();
 
     var filterspanel = $('<div id="filterspanel" class="filterspanel"></div>');
