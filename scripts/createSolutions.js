3e347f0cb769fafd080ff1f74f5c9eae1ef556e8
Tobi <40991903+tobka777@users.noreply.github.com>
2021-12-14 15:54:14 +0100

    #83 rename solutions subfolder to solution

--- scripts/createSolutions.js
+++ scripts/createSolutions.js
@@ -28,14 +28,14 @@ function main(solutionsDir, outputFile, snippetLength) {
                     sharp(`./solutions/${dirItem}/${imagePath}`)
                         .resize({ width: 600 })
                         .toFile(`./target/generated-docs/solutions/${dirItem}/_previewImage.png`);
-                    imagePath = `./solutions/${dirItem}/_previewImage.png`;
+                    imagePath = `./solution/${dirItem}/_previewImage.png`;
                 }
 
                 console.log("After:" + imagePath);
                 
                 solutions[dirItem] = {
                     headline: headline,
-                    path: `solutions/${dirItem}`,
+                    path: `solution/${dirItem}`,
                     image: imagePath,
                     snippet: snippet
                 }
